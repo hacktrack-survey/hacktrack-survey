254fc85e777cf723c86e771cc9580cb25050260f
sixonenines <49105737+sixonenines@users.noreply.github.com>
2022-07-03 16:35:00 +0200

    Update emailController.js

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -16,7 +16,7 @@ function sendFormCreationMail(formMetaData, formUrl, formUuid, dashboard_url) {
   let subject = `${formMetaData.organizer[0]}, your Google Form is here!`;
   ejs.renderFile("./views/email.ejs",{hackathon_name: formMetaData.name,hackathon_org:formMetaData.organizer[0],form_url:formUrl,dashboard_url:dashboard_url,form_uuid:formUuid},function (err,data) {
   var mainOptions = {
-    from: '"Hackathon planning kit" <test@sixonenine.club>',
+    from: '"Hackathon Planning Kit" <test@sixonenine.club>',
     to: formMetaData.email,
     subject: subject, // Subject line
     html: data // html body
