89ed7c53ce209132f4f64096cc91eafcd2a72a5a
sixonenines <49105737+sixonenines@users.noreply.github.com>
2022-07-03 16:01:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const apiRouter = require("./routes/apiRouter");
 
 const app = express();
 
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
