8881300ffd188399fa1dc44c572f8ea4333b79c3
sixonenines <49105737+sixonenines@users.noreply.github.com>
2022-07-04 18:39:26 +0200

    Update emailController.js

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -3,11 +3,11 @@ const ejs=require('ejs');
 
 
 let transporter = nodemailer.createTransport({
-  host:"smtp-relay.sendinblue.com",
-  port: 587,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   secure: false,
   auth: {
-    user: 'test@sixonenine.club',
+    user: process.env.EMAIL,
     pass: process.env.EMAIL_WP,
   },
 });
@@ -16,7 +16,7 @@ function sendFormCreationMail(formMetaData, formUrl, formUuid, dashboard_url) {
   let subject = `${formMetaData.organizer[0]}, your Google Form is here!`;
   ejs.renderFile("./views/email.ejs",{hackathon_name: formMetaData.name,hackathon_org:formMetaData.organizer[0],form_url:formUrl,dashboard_url:dashboard_url,form_uuid:formUuid},function (err,data) {
   var mainOptions = {
-    from: '"Hackathon Planning Kit" <test@sixonenine.club>',
+    from: `"Hackathon Planning Kit" <${process.env.EMAIL}>`,
     to: formMetaData.email,
     subject: subject, // Subject line
     html: data // html body
